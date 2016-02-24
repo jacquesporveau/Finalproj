189bd9ebb0fcb571e78de0b1db8f81e8bdfaa34e
Dennis Marchand <dennis@urbantherapy.ca>
2016-02-24 13:18:37 -0800

    Several routes built and sending JSON to front end

--- app/controllers/smoothies_controller.rb
+++ app/controllers/smoothies_controller.rb
@@ -1,4 +1,32 @@
 class SmoothiesController < ApplicationController
-   def index
-   end
- end
+  def index
+  end
+
+  def nutritionaljson
+    render json: {
+    'width' => 280,
+    'showServingUnitQuantity' => false,
+    'itemName' => 'Dog shit',
+    'ingredientList' => 'Dog shit',
+
+    'showPolyFat' => false,
+    'showMonoFat' => false,
+
+    'valueCalories' => 450,
+    'valueFatCalories' => 430,
+    'valueTotalFat' => 48,
+    'valueSatFat' => 6,
+    'valueTransFat' => 0,
+    'valueCholesterol' => 30,
+    'valueSodium' => 780,
+    'valueTotalCarb' => 3,
+    'valueFibers' => 0,
+    'valueSugars' => 3,
+    'valueProteins' => 3,
+    'valueVitaminA' => 0,
+    'valueVitaminC' => 0,
+    'valueCalcium' => 0,
+    'valueIron' => 0
+  }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
-Finalproj::Application.routes.draw do
+ Finalproj::Application.routes.draw do
+  
   root to: 'smoothies#index'
+
+  get '/nutritional' => 'smoothies#nutritional'
+  get '/nutritionaljson' => 'smoothies#nutritionaljson'
+
 end
