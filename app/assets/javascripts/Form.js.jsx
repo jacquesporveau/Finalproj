89237e3b4d7ee5974906fbf992167580b57e6890
Dennis Marchand <dennis@urbantherapy.ca>
2016-02-24 13:06:08 -0800

    Langing page HTML +CSS added

---
+++ app/assets/javascripts/Form.js.jsx
@@ -0,0 +1,105 @@
+var Form = React.createClass({
+  render: function() {
+    return (
+    <div className="form-contain">
+      <form acceptCharset="UTF-8" action="" className="search-form" method="get">
+      <label className="form-label "for="fruit_query">Main Fruit Ingredient</label>
+      <select className="form-entry" name="fruit">
+        <option selected disabled>- Please Select -</option>
+        <option>Dates</option>
+        <option>Goji Berries</option>
+        <option>Blue Berries</option>
+        <option>Starwberries</option>
+        <option>Bananas</option>
+        <option>Raspberries</option>
+        <option>Apples</option>
+        <option>Pears</option>
+        <option>Blackberries</option>
+        <option>Plums</option>
+        <option>Peaches</option>
+        <option>Mango</option>
+        <option>Pineapple</option>
+        <option>Acai Berries</option>
+        <option>Nectarines</option>
+        <option>Plums</option>
+      </select>
+      <label className="form-label "for="veg_query">Main Veggie Ingredient</label>
+      <select className="form-entry" name="veg">
+        <option selected disabled>- Please Select -</option>
+        <option>Kale</option>
+        <option>Swiss Chard</option>
+        <option>Red Beets</option>
+        <option>Carrots</option>
+        <option>Cucumber</option>
+        <option>Spinach</option>
+        <option>Watercress</option>
+        <option>Arugula</option>
+        <option>Celery</option>
+        <option>Fennel</option>
+        <option>Kelp</option>
+        <option>Bok Choi</option>
+        <option>Peppers</option>
+      </select>
+      <label className="form-label "for="liquid_query">Liquid/Base</label>
+      <select className="form-entry" name="liqud">
+        <option selected disabled>- Please Select -</option>
+        <option>Water</option>
+        <option>Almond</option>
+        <option>Milk (2%)</option>
+        <option>Skim Milk</option>
+        <option>Soy Milk</option>
+        <option>Coconut Water</option>
+        <option>Apple Juice</option>
+        <option>Mango Juice</option>
+        <option>Berry Juice</option>
+        <option>Lemon Juice</option>
+        <option>Lime Juice</option>
+      </select>
+      <label className="form-label "for="seed-nut_query">Seed/Nut Ingredient</label>
+      <select className="form-entry" name="seeds">
+        <option selected disabled>- Please Select -</option>
+        <option>Chia</option>
+        <option>Flax</option>
+        <option>Hemp</option>
+        <option>Pumpkin</option>
+        <option>Sunflower</option>
+        <option>Almonds</option>
+        <option>Brazil Nuts</option>
+        <option>Almonds</option>
+        <option>Walnuts</option>
+        <option>Cacao</option>
+        <option>Sachainchi</option>
+      </select>
+      <label className="form-label "for="herb_query">Main Herb/Flavouring Ingredient</label>
+      <select className="form-entry" name="herbs">
+        <option selected disabled>- Please Select -</option>
+        <option>Mint</option>
+        <option>Basil</option>
+        <option>Parsley</option>
+        <option>Ginger</option>
+        <option>Garlic</option>
+        <option>Honey</option>
+        <option>Kaffir Leaves</option>
+        <option>Tumeric</option>
+        <option>Lemongrass</option>
+        <option>Cinnamon</option>
+        <option>Lemon Juice</option>
+        <option>Lime Juice</option>
+        <option>Black Pepper</option>
+        <option>Honey</option>
+        <option>Brown Sugar</option>
+        <option>Sugar</option>
+      </select>
+        <p className="carry-on">Almost done! Just enter your information below and we'll send you your grade</p>
+        <label className="form-label "for="firstname_query">First Name</label>
+        <input className="form-entry" name="firstname_query" type="text" />
+        <label className="form-label "for="lastname_query">Last Name</label>
+        <input className="form-entry" name="lastname_query" type="text" />
+        <label className="form-label "for="email_query">Email</label>
+        <input className="form-entry" name="email_query" type="text" />
+        <input className="btn-grade" type="submit" value="GRADE MY SMOOTHIE" />
+      </form>
+    </div>
+    )
+  }
+})
