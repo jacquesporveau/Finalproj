f77e2a132a1c5a4c4bc9b1d1e3bccefeeb06d405
Dennis Marchand <dennis@urbantherapy.ca>
2016-02-24 12:34:52 -0800

    React connected, basic functionality

---
+++ app/assets/javascripts/Nav.js.jsx
@@ -0,0 +1,22 @@
+var Nav = React.createClass({
+  render: function() {
+    return (
+      <header className="site-header">
+        <a href="https://smoothieveggies.com">
+          <img className="site-logo" src="https://smoothieveggies.com/wp-content/uploads/2015/07/sv_logo_white.png" />
+        </a>
+        <div className="btn-menu">
+          <nav>
+            <ul>
+              <a href="https://smoothieveggies.com/our-story/" className="dropdown"><li>About</li></a>
+              <a href="https://smoothieveggies.com/faqs/"><li>FAQs</li></a>
+              <a href="https://smoothieveggies.com/recipes/"><li>Recipes</li></a>
+              <a href="https://smoothieveggies.com/contact/"><li>Contact</li></a>
+              <a href="https://smoothieveggies.com/hibernation/"><li>Order</li></a>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    )
+  }
+})
