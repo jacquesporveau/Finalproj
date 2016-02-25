c079c6a798003e05e94cc942dbffff64535a85d9
Dennis Marchand <dennis@urbantherapy.ca>
2016-02-24 16:56:04 -0800

    Database queries and logic

--- app/controllers/smoothies_controller.rb
+++ app/controllers/smoothies_controller.rb
@@ -3,30 +3,42 @@ class SmoothiesController < ApplicationController
   end
 
   def nutritionaljson
+
+    @carbs = Nutrient.where(name: 'Carbohydrate, by difference').limit(1).pluck(:value)
+    @fat = Nutrient.where(name: 'Total lipid (fat)').limit(1).pluck(:value)
+    @protein = Nutrient.where(name: 'Protein').limit(1).pluck(:value)
+    # Calories = Carbs * 4 + Fat * 9 + Protein * 4
+    @cals = (@carbs[0] * 4) + (@fat[0] * 9) + (@protein[0] * 4)
+    @cals_from_fat = @fat[0] * 9
+    @vitC = Nutrient.where(name: 'Vitamin C, total ascorbic acid').limit(1).pluck(:value)
+    @vitA = Nutrient.where(name: 'Vitamin A, RAE').limit(1).pluck(:value)
+    @calcium = Nutrient.where(name: 'Calcium, Ca').limit(1).pluck(:value)
+    @iron = Nutrient.where(name: 'Iron, Fe').limit(1).pluck(:value)
+
     render json: {
     'width' => 280,
     'showServingUnitQuantity' => false,
-    'itemName' => 'Dog shit',
-    'ingredientList' => 'Dog shit',
-
+    'itemName' => 'Smoothie',
+    'ingredientList' => "dog shit",
     'showPolyFat' => false,
     'showMonoFat' => false,
 
-    'valueCalories' => 450,
-    'valueFatCalories' => 430,
-    'valueTotalFat' => 48,
-    'valueSatFat' => 6,
+    'valuePotassium' => Nutrient.where(name: 'Potassium, K').limit(1).pluck(:value),
+    'valueCalories' => @cals,
+    'valueFatCalories' => @cals_from_fat,
+    'valueTotalFat' => @fat,
+    'valueSatFat' => 0,
     'valueTransFat' => 0,
-    'valueCholesterol' => 30,
-    'valueSodium' => 780,
-    'valueTotalCarb' => 3,
-    'valueFibers' => 0,
-    'valueSugars' => 3,
-    'valueProteins' => 3,
-    'valueVitaminA' => 0,
-    'valueVitaminC' => 0,
-    'valueCalcium' => 0,
-    'valueIron' => 0
+    'valueCholesterol' => Nutrient.where(name: 'Cholesterol').limit(1).pluck(:value),
+    'valueSodium' => Nutrient.where(name: 'Sodium, Na').limit(1).pluck(:value),
+    'valueTotalCarb' => Nutrient.where(name: 'Fiber, total dietary').limit(1).pluck(:value),
+    'valueFibers' => Nutrient.where(name: 'Cholesterol').limit(1).pluck(:value),
+    'valueSugars' => Nutrient.where(name: 'Sugars, total').limit(1).pluck(:value),
+    'valueProteins' => @protein,
+    'valueVitaminA' => @vitA[0].to_f / 3000,
+    'valueVitaminC' => @vitC[0].to_f / 85,
+    'valueCalcium' => @calcium[0].to_f / 1000,
+    'valueIron' => @iron[0].to_f / 16
   }
   end
 end
