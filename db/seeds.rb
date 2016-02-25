f94a48b37e0c6da31bf614f3be4908ea62f139bf
Connor <connorsteele604@gmail.com>
2016-02-25 13:32:33 -0800

    ignore database.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,16 @@ require 'csv'
 
 # WATCH CSV NAMING ADDRESS.
 
-CSV.foreach('/Users/dennismarchand/lighthouse/finalproject/finalproj2/finalproj/db/foodCSV/berries/banana1.csv', encoding: "ISO8859-1") do |row|
-  break if row[0] == "Footnotes"
-  # def clean_value(num)
-  #   num.slice(4, 5000).join(',').encode('UTF-16le', invalid: :replace, replace: '').encode('UTF-8') unless num.nil?
-  # end
-  # encoded_value = clean_value(row[2])
-  Nutrient.create({name: row[0], value: row[2], unit: row[1]})
+require 'csv'
+users_csvs = Dir['banana1.csv']
+users_csvs.each do |user_file|
+  CSV.foreach(user_file, encoding: "ISO8859-1") do |row|
+    break if row[0] == "Footnotes"
+    puts row[0]
+    # def clean_value(num)
+    #   num.slice(4, 5000).join(',').encode('UTF-16le', invalid: :replace, replace: '').encode('UTF-8') unless num.nil?
+    # end
+    # encoded_value = clean_value(row[2])
+    Nutrients.create({name: row[0], value: row[2], unit: row[1]})
+  end
 end
