af058560ca2fe47342d4622e4f53db7bcc6dde63
Dennis <dennis@urbantherapy.com>
2016-02-26 15:19:53 -0800

    Form now allows blank values

--- app/assets/javascripts/nutritional.js
+++ app/assets/javascripts/nutritional.js
@@ -1,9 +1,31 @@
 $( document ).ready(function() {
+
   var fruit = $('#fruit').val();
   var veg = $('#veg').val();
   var liquid = $('#liquid').val();
   var seednut = $('#seednut').val();
   var herb = $('#herb').val();
+
+  if (fruit == "") {
+    fruit = "default";
+  }
+
+  if (veg == "") {
+    veg = "default";
+  }
+
+  if (liquid == "") {
+    liquid = "default";
+  }
+
+  if (seednut == "") {
+    seednut = "default";
+  }
+
+  if (herb == "") {
+    herb = "default";
+  }
+
   $.get( "/nutritionaljson?fruit="+fruit+"&veg="+veg+"&liquid="+liquid+"&seednut="+seednut+"&herb="+herb, function( data ) {
     $('#nutritional-label').nutritionLabel(data);
   });
